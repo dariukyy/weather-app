abce0671aa43371a20e87c018e39d03d6e3b84da
Darius <dpdarius1@gmail.com>
2024-03-14 12:18:38 +0200

    added cityPicker and install dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
+import CityPicker from "@/components/CityPicker";
+import { Card, Divider, Subtitle, Text, Title } from "@tremor/react";
+
 export default function Home() {
   return (
-    <main className="text-6xl">
-      <h1> Lets Build a GPT-4 weather app</h1>
-    </main>
+    <div className="min-h-screen bg-gradient-to-br from-[#394F68] to-[#183B7E] p-10 flex flex-col justify-center items-center">
+      <Card className="max-w-3xl mx-auto rounded-md">
+        <Title className="text-5xl font-bold text-center mb-10">
+          Dariukyy Weather AI
+        </Title>
+        <Subtitle className="text-xl text-center">
+          Powered by OpenAI, Next.js 14.2, Tailwind CSS, Tremor 3.14 + More!
+        </Subtitle>
+        <Divider className="my-10" />
+        <Card className="rounded-lg bg-gradient-to-br from-[#394F68] to-[#183B7E]">
+          <CityPicker />
+        </Card>
+      </Card>
+    </div>
   );
 }
