46ede8236235669012636a1e76bb207b339e3826
Darius <dpdarius1@gmail.com>
2024-03-14 18:24:48 +0200

    edit API KEY

--- apolo-client.ts
+++ apolo-client.ts
@@ -1,17 +1,13 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-let client: ApolloClient<any | null>;
-
 export const getClient = () => {
-  if (!client) {
-    client = new ApolloClient({
-      uri: process.env.API_URL,
-      cache: new InMemoryCache(),
-      headers: {
-        Authorization: `api key ${process.env.NEXT_PUBLIC_STEPZEN_API_KEY}`,
-      },
-    });
-  }
+  const client = new ApolloClient({
+    uri: process.env.API_URL,
+    cache: new InMemoryCache(),
+    headers: {
+      authorization: `Apikey ${process.env.NEXT_PUBLIC_STEPZEN_API_KEY}`,
+    },
+  });
 
   return client;
 };
